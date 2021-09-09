55d5291ae21383902527226d1b679d838c3124e1
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-09 16:43:36 +0200

    Adding lesson 2

---
+++ Lesson02IntroToTryParse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02IntroToTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value1 = "454";
+            int parsedValue1;
+
+            bool success = int.TryParse(value1, out parsedValue1);
+
+            if(success)
+            {
+                Console.WriteLine($"Parsing successful, the number is {parsedValue1}.");
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed.");
+            }
+
+            string value2 = "45.454ee";
+            float parsedValue2;
+
+            bool successful = float.TryParse(value2, out parsedValue2);
+
+            if(successful)
+            {
+                Console.WriteLine($"Parsing successful, the number is {parsedValue2}");
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed.");
+            }
+
+            Console.Read();
+        }
+    }
+}
