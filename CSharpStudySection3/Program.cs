515973bcfb9b07c4bc8554ff4db05383d5335d4c
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-09 16:12:11 +0200

    Adding lesson 1

--- CSharpStudySection3/Program.cs
+++ CSharpStudySection3/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpStudySection3
+namespace Lesson01Introduction
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("What is the temperature outside? ");
+            int temperature = int.Parse(Console.ReadLine());
+
+            if(temperature < 10)
+            {
+                Console.WriteLine("Take the coat");
+            }
+            else if(temperature < 20)
+            {
+                Console.WriteLine("It's mild");
+            }
+            else
+            {
+                Console.WriteLine("It is warm");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
