1641119e835a8c660fe2511f2e474ce3eefe341d
Nikola Burgić <nikola.burgic@wm.rs>
2021-09-14 12:33:11 +0200

    Adding lesson 7

---
+++ Lesson07TernaryOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson07TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int temperature = -5;
+            string stateOfMatter;
+
+            if(temperature <= 0)
+            {
+                stateOfMatter = "solid";
+            }
+            else if(temperature < 100)
+            {
+                stateOfMatter = "liquid";
+            }
+            else
+            {
+                stateOfMatter = "gas";
+            }
+
+            Console.WriteLine($"State of matter is {stateOfMatter}.");
+
+            Console.Read();
+        }
+    }
+}
